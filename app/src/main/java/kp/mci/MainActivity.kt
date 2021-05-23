da97392ab83dfdbff3306f91772e449ab5b59381
Robert Liefke <robert-liefke@gmx.de>
2021-05-23 20:56:59 +0200

    added standart layout

--- app/src/main/java/kp/mci/MainActivity.kt
+++ app/src/main/java/kp/mci/MainActivity.kt
@@ -1,4 +1,11 @@
 package kp.mci
 
-class MainActivity {
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+        override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        }
 }
