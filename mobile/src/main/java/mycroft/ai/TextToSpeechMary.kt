057e31c393aca4355fcfe3c81c0bd1c1a185e6b9
Thomas Pattoka <thomas.pattoka@mailbox.tu-dresden.de>
2021-06-28 20:24:45 +0200

    added new play function, now supports multiple responses from mycroft

--- mobile/src/main/java/mycroft/ai/TextToSpeechMary.kt
+++ mobile/src/main/java/mycroft/ai/TextToSpeechMary.kt
@@ -5,6 +5,7 @@ import android.media.MediaPlayer
 import android.net.Uri
 import android.os.Build
 import androidx.annotation.RequiresApi
+import com.android.volley.DefaultRetryPolicy
 import com.android.volley.Request
 import com.android.volley.RequestQueue
 import com.android.volley.Response
@@ -15,6 +16,8 @@ import mycroft.ai.shared.utilities.GuiUtilities.showToast
 import java.io.File
 import java.io.FileOutputStream
 import java.io.InputStream
+import java.net.URL
+import java.nio.file.Files
 import java.nio.file.Path
 import java.util.*
 import java.util.Base64
@@ -25,9 +28,10 @@ class TextToSpeechMary {
     private var queue : RequestQueue
     private var url : String
     private var port : String
-    private var path : File
-    private var file : File
-    private var mediaPlayer : MediaPlayer?
+    private var filePath : File
+    private var mediaPlayer : MediaPlayer = MediaPlayer()
+    private var files : Queue<File> = LinkedList<File>()
+    private var currentOutput = 0
 
     constructor(context : Context, serverIp : String, port : String = "59125") {
         this.serverIp = serverIp
@@ -35,57 +39,68 @@ class TextToSpeechMary {
         this.port = port
         queue = Volley.newRequestQueue(context)
         url = "http://$serverIp:$port"
-        path = context.filesDir
-        file = File(path, "output.wav")
-        mediaPlayer = MediaPlayer.create(context, Uri.fromFile(file))
+        filePath = context.filesDir
     }
 
     fun sendTTSRequest(input_text : String) {
-        val mUrl = "$url/process?INPUT_TEXT=$input_text&INPUT_TYPE=TEXT&OUTPUT_TYPE=AUDIO&AUDIO=WAVE_FILE&LOCALE=de&VOICE=bits3-hsmm"
-        //var mUrl = "$url/api/tts?text=$input_text&voice=de-de%2Fthorsten-glow_tts&vocoder=hifi_gan%2Fvctk_medium&denoiserStrength=0.005&noiseScale=0.333&lengthScale=1"
-        val hashMap: HashMap<String, String> = HashMap()
-        val request = InputStreamVolleyRequest(
+        // MaryTTS
+        // var mUrl = "$url/process?INPUT_TEXT=$input_text&INPUT_TYPE=TEXT&OUTPUT_TYPE=AUDIO&AUDIO=WAVE_FILE&LOCALE=de&VOICE=bits3-hsmm"
+        // Larynx with Eva-Voice
+        // var mUrl = "$url/api/tts?text=$input_text&voice=de-de/eva_k-glow_tts&vocoder=hifi_gan/vctk_medium&denoiserStrength=0.005&noiseScale=0.333&lengthScale=1"
+        // Larynx with Karlsson-Voice
+        var mUrl = "$url/api/tts?text=$input_text&voice=de-de/karlsson-glow_tts&vocoder=hifi_gan/vctk_medium&denoiserStrength=0.005&noiseScale=0.333&lengthScale=1"
+        var hashMap: HashMap<String, String> = HashMap()
+        var request = InputStreamVolleyRequest(
             context, Request.Method.GET, mUrl,
             Response.Listener<ByteArray>() { response ->
                 writeWavFile(response)
             },
-            Response.ErrorListener { showToast(context, "That didn't work!") },
+            Response.ErrorListener { error ->
+                showToast(context, error.toString()) },
             hashMap)
+        request.retryPolicy = DefaultRetryPolicy(10000, 2, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT)
         queue.add(request);
     }
 
     private fun writeWavFile(data : ByteArray) {
-        file = File(path, "output.wav")
+        val file = File(filePath, "output$currentOutput.wav")
+        currentOutput++
         FileOutputStream(file).use {
             it.write(data)
         }
-        playWav(file)
+        files.add(file)
+        //playWav(file)
+        play()
     }
 
-    private fun playWav(file : File) {
-        if (mediaPlayer == null) {
-            mediaPlayer = MediaPlayer.create(context, Uri.fromFile(file))
-        }
-        if (mediaPlayer?.isPlaying == true) {
+    private fun playWav(file: File) {
+        //val mediaPlayer = MediaPlayer.create(context, Uri.fromFile(file))
 
-            mediaPlayer?.setOnCompletionListener {
-                try{
-                    mediaPlayer?.reset()
-                    mediaPlayer?.setDataSource(file.path)
-                    mediaPlayer?.prepare()
-                    mediaPlayer?.start()
-                    file.delete()
-                } catch (e : Exception) {
-                }
-            }
+        if (!mediaPlayer.isPlaying) {
+            mediaPlayer.setDataSource(file.path)
+            mediaPlayer.prepare()
+            mediaPlayer.start()
         } else {
-            mediaPlayer?.reset()
-            mediaPlayer?.setDataSource(file.path)
-            mediaPlayer?.prepare()
-            mediaPlayer?.start()
-            file.delete()
+            showToast(context, "Waiting for MediaPlayer to finish")
+        }
+        mediaPlayer.reset()
+        mediaPlayer.setDataSource(file.path)
+        mediaPlayer.prepare()
+        mediaPlayer.start()
+    }
+
+    private fun play() {
+        if(!mediaPlayer.isPlaying && !files.isEmpty()) {
+            mediaPlayer.reset()
+            mediaPlayer.setDataSource(files.remove().path)
+            mediaPlayer.prepare()
+            mediaPlayer.start()
         }
 
+        // when WAV is received while audio is still playing
+        if (mediaPlayer.isPlaying && !files.isEmpty()) {
+            mediaPlayer.setOnCompletionListener(MediaPlayer.OnCompletionListener { play() })
+        }
     }
 
 }
